fd42ad46beb5708b47da85118fbd879fb8c896a8
Hammertonmutuku <lthamma3@gmail.com>
2019-12-09 10:49:02 +0300

    generated a apipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubComponent
+    GithubComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
