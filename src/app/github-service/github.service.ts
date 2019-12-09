a48a911cf72a2a28255288df55eac4610462ed02
Hammertonmutuku <lthamma3@gmail.com>
2019-12-09 11:47:57 +0300

    updated all the information in the get profile

--- src/app/github-service/github.service.ts
+++ src/app/github-service/github.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
-
-  constructor() { }
+  private username:string;
+ private client_id = "0f1b981f0644fe1b074f";
+  private client_secret ="54a0e4b62936a3bf0d4bfb8a1dea9aa1ee1ef1e7";
+  constructor(private http:HttpClient) { 
+    this.username = "hammertonmutuku";
+  }
+  getgithubInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.client_id + "&client_secret=" + this.client_secret)
+  }
+  getProfileRepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id="+ this.client_id + "&client_secret=" + this.client_secret)
+  }
+  updateProfile(username:string){
+    this.username = username;
+  }
 }
+
--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import {   GithubService }from '../github-service/github.service';
 @Component({
   selector: 'app-github',
   templateUrl: './github.component.html',
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
+    profile:any;
+    repos:any;
+    username:string;
 
-  constructor() {}
-
-  ngOnInit() {
-  }
-
-}
+    constructor(private profileService: GithubService){
+      this.profileService.updateProfile(this.username);
+      this.profileService.getProfileInfo().subscribe(profile => {
+        console.log(profile);
+        this.profile = profile;
+      });
+    
+      this.profileService.getProfileRepos().subscribe(repos => {
+        console.log(repos);
+        this.repos = repos;
+      });
+    }
+     
+    findProfile(){
+      this.profileService.updateProfile(this.username);
+      this.profileService.getProfileInfo().subscribe(profile => {
+        console.log(profile);
+        this.profile = profile;
+      });
+    
+      this.profileService.getProfileRepos().subscribe(repos => {
+        console.log(repos);
+        this.repos = repos;
+      })
+    }
+      ngOnInit() {
+        
+        this.profileService.updateProfile('');
+        this.profileService.getProfileInfo().subscribe(profile => this.profile = profile);
+        this.profileService.getProfileRepos().subscribe(repos =>  this.repos = repos);
+      }
+    
+    }
